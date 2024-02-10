13cefe6ec8b1c052c64203d7ae87293cae2e2abd
iSandRocks <sean.h1313@gmail.com>
2024-02-10 18:52:31 -0500

    Vial Implemented

--- causeway_QMK/keymaps/default/keymap.c
+++ causeway_QMK/keymaps/default/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5, KC_F6,   KC_F7,   KC_F8,  KC_F9,  KC_F10,  KC_F11,  KC_F12,   KC_DEL,
         KC_TAB,  KC_Q,    KC_UP,   KC_E,    KC_R,    KC_T,  KC_LBRC, KC_RBRC, KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,     KC_BSLS,
         KC_CAPS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_F,   KC_G,  KC_QUOT, KC_GRV,  KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN,  KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_DEL,  KC_END,  KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLASH, KC_RSFT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,  KC_KB_VOLUME_UP,  KC_KB_VOLUME_DOWN,  KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLASH, KC_RSFT,
         KC_LCTL, KC_LGUI, KC_APP,  KC_LALT, KC_SPC,  KC_SPC,         KC_SPC,  KC_SPC, KC_SPC, KC_RALT, KC_RGUI, MO(1),    KC_RCTL
         )
       };
--- causeway_QMK/rules.mk
+++ causeway_QMK/rules.mk
@@ -16,7 +16,7 @@ F_CPU = 16000000
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
+MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no        # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
